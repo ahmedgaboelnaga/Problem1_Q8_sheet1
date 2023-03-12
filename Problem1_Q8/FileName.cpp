7e8a38bfb1190c54089286884f56a799abd1b251
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 22:06:28 +0200

    comment

--- Problem1_Q8/FileName.cpp
+++ Problem1_Q8/FileName.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
+    //(Population projection) The U.S. 
+    //Census Bureau projects population based on the following assumptions: (Lab)■■ One birth every 7 seconds ■■ One death every 13 seconds ■■ One new immigrant every 45 seconds.
+    //Write a program to display the population for each of the next five years. Assume that the current population is 312,032,486, and one year has 365 days. 
+
     int current_population;
     cout << "What is the current popualtion: ";
     cin >> current_population;
